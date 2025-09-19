c582f89c63c5c56797fc9c76541cc5403604f60b
md ismail <mdismail85773@gmail.com>
2025-09-20 02:02:27 +0800

    form input validatin check function added

--- src/components/product/ProductFrom.jsx
+++ src/components/product/ProductFrom.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ProductFrom = ({addProduct}) => {
+    const [error, setError] = useState('')
 
   const handleForm = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
     const price = e.target.price.value;
     const qtn = e.target.gtn.value;
-
 //   console.log(name, price, qtn)
+// validation part
+    if(name ===0){
+        setError("Please provide valid name ")
+        return;
+    }
+    else if(price <= 0 ){
+        setError("Please provide valid price")
+        return;
+    }
+    else if(qtn <= 0){
+        setError("Please provide valid quantity")
+        return;
+    }
+    else{
+        setError('')
+    }
 
+    
 const newProduct={
         name, price, qtn
     }
@@ -25,6 +42,7 @@ const newProduct={
                 <input type="number" name='gtn' placeholder='Quantity'className='border p-2 rounded-2xl' /> <br/>
                 <button className='border p-2 rounded-2xl'>Submit</button>
             </form>
+            <h1>{error}</h1>
         </div>
     );
 };
--- src/components/product/ProductTable.jsx
+++ src/components/product/ProductTable.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 
 const ProductTable = ({product}) => {
-    // console.log(product)
+    console.log(product)
     return (
         <div>
-            {
-                product.map(product => )
-            }
+            <h1>{product.length}</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>No:</th>
+                        <th>Product Name:</th>
+                        <th>Product Price:</th>
+                        <th>Product Qtn:</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        product.map((product,index) => <tr key={index} product={product}>
+                            <td>{index+1}</td>
+                            <td>{product.name}</td>
+                            <td>{product.price}</td>
+                            <td>{product.qtn}</td>
+                            <td></td>
+                        </tr>)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
